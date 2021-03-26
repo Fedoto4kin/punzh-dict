918f5dc457ebd8b76deff4fbdf1a51feda8d3c8f
FAnatole <fedoto4kin@gmail.com>
2021-03-26 15:46:51 +0300

    Add model for Articule

--- app/dict/models.py
+++ app/dict/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+KRL_ABC = 'ABCČDEFGHIJKLMNOPRSŠZŽTUVÜÄÖ'
+
+class Article(models.Model):
+
+    word = models.CharField(max_length=255)
+    word_slug = models.CharField(max_length=255)
+    first_letter = models.CharField(max_length=1)
+    first_trigram = models.CharField(max_length=3)
+    definition = models.TextField()
+
