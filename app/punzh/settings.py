002b839bae9bd456f0c912d5fe45366f2da94bbe
FAnatole <fedoto4kin@gmail.com>
2021-03-26 15:47:36 +0300

    Add dict app to project

--- app/punzh/settings.py
+++ app/punzh/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'dict.apps.DictConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
