fe66c6311983e33669fa4f0fe64cdf123ee18516
FAnatole <fedoto4kin@gmail.com>
2021-03-27 13:18:28 +0300

    Model update; create view

--- app/dict/models.py
+++ app/dict/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from slugify import slugify
 
 KRL_ABC = 'ABCČDEFGHIJKLMNOPRSŠZŽTUVÜÄÖ'
 
@@ -8,5 +9,28 @@ class Article(models.Model):
     word_slug = models.CharField(max_length=255)
     first_letter = models.CharField(max_length=1)
     first_trigram = models.CharField(max_length=3)
-    definition = models.TextField()
+    article_html = models.TextField(default='')
 
+    @staticmethod
+    def get_krl_abc():
+        abc = ''
+        for l in KRL_ABC:
+            abc += l + l.lower()
+        return abc
+
+    def __str__(self):
+        return self.word
+
+    def save(self, *args, **kwargs):
+        self.word_slug = slugify(self.word)
+        self.first_letter = self.word[0].upper()
+        # create first trigram from word 
+        self.first_trigram = ''.join(
+            list(
+                filter( 
+                        lambda x: x in self.get_krl_abc(), 
+                        self.word #todo: splite by whitespace
+                    )
+            )
+        )[:3].lower()
+        super(Article, self).save(*args, **kwargs)
