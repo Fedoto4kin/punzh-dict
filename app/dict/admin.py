719969548ad927d0f82a249fb3096f407485f429
FAnatole <fedoto4kin@gmail.com>
2021-03-28 22:06:03 +0300

    Admin panel view for Article

--- app/dict/admin.py
+++ app/dict/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.utils.html import strip_tags
 
 from .models import Article
 
-admin.site.register(Article)
+
+class ArticleAdm(admin.ModelAdmin):
+
+    list_display = ('word', '_article_html')
+    search_fields = ('word',)
+    readonly_fields = ('word_slug',)
+    exclude = ("first_letter", "first_trigram")
+
+    @staticmethod
+    def _article_html(self):
+        return strip_tags(self.article_html)
+
+
+admin.site.register(Article, ArticleAdm)
