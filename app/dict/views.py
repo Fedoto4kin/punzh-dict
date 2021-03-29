a4599aff6992889f854c22f2a85fc0e642dca282
FAnatole <fedoto4kin@gmail.com>
2021-03-29 23:09:45 +0300

    Add alphabeth pointer

--- app/dict/views.py
+++ app/dict/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Article, KRL_ABC
 
-def index(request):
-    return HttpResponse("Terveh")
+def index(request, letter=''):
+
+    articles = []
+
+    if len(letter):
+        articles = Article.objects.all().filter(first_letter=letter.upper())
+
+    context = {
+        "ABC": KRL_ABC,
+        "articles": articles,
+        "letter": letter.upper()
+    }
+
+    return render(request, 'article_list.html', context)
