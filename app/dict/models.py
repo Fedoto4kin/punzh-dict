645de149b2b1353d9b79ba867ce1444ca93643fb
FAnatole <fedoto4kin@gmail.com>
2021-03-29 23:08:42 +0300

    Add index for first letter

--- app/dict/models.py
+++ app/dict/models.py
@@ -7,10 +7,9 @@ class Article(models.Model):
 
     word = models.CharField(unique=True, max_length=255, db_index=True)
     word_slug = models.CharField(max_length=255)
-    first_letter = models.CharField(max_length=1)
+    first_letter = models.CharField(max_length=1, db_index=True)
     first_trigram = models.CharField(max_length=3)
     article_html = models.TextField(default='')
-
 
     @staticmethod
     def get_krl_abc():
@@ -23,6 +22,7 @@ class Article(models.Model):
         return self.word
 
     def save(self, *args, **kwargs):
+
         self.word_slug = slugify(self.word)
         self.first_letter = self.word[0].upper()
         # create first trigram from word 
@@ -32,6 +32,6 @@ class Article(models.Model):
                         lambda x: x in self.get_krl_abc(),
                         self.word #todo: splite by whitespace
                     )
-            )
-        )[:3].lower()
+                )
+            )[:3].lower()
         super(Article, self).save(*args, **kwargs)
