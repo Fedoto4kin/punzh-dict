dccfd74e273600bd3892d7747072f9c2f83aa52e
FAnatole <fedoto4kin@gmail.com>
2021-04-07 22:54:20 +0300

    Search v.1

--- app/dict/templatetags/dict_extras.py
+++ app/dict/templatetags/dict_extras.py
@@ -4,14 +4,14 @@ import re
 register = template.Library()
 
 
-@register.filter(name='nice')
+@register.filter
 def nice(text):
     # TODO: only in <b> tag and remove ||
     return text.replace('||', '<span class="text-muted font-weight-normal">||</span>') \
                .replace('|', '<span class="text-muted font-weight-normal">|</span>')
 
 
-@register.filter(name='clear')
+@register.filter
 def clear(text):
     base = re.split(r'\|+', text.split(',')[0])[0]
     return text.replace('~', base.strip()) \
@@ -21,3 +21,8 @@ def clear(text):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def highlight(text, search):
+    return text.replace(search, '<span class="bg-warning">' + search + '</span>')
