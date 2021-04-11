224b155c0766bb90e9a105b6357595efcdffa84d
FAnatole <fedoto4kin@gmail.com>
2021-04-11 08:12:21 +0300

    Differnet color for Russian language

--- app/dict/templatetags/dict_extras.py
+++ app/dict/templatetags/dict_extras.py
@@ -6,18 +6,17 @@ register = template.Library()
 
 @register.filter
 def nice(text):
-    # TODO: only in <b> tag and remove ||
-    return text.replace('||', '<span class="text-muted font-weight-normal">||</span>') \
-               .replace('|', '<span class="text-muted font-weight-normal">|</span>')
-
+    # TODO: only in <b> tags
+    return re.sub(r'([\|]{1,2})', r'<span class="text-muted font-weight-normal">\1</span>', text)
 
 
 @register.filter
 def clear(text):
-    base = re.split(r'\|+', text.split(',')[0])[0]
+    base = re.split(r'\|+', text.split(',')[0])[0] # TODO: rewrite with re.sub or re.find
     return text.replace('~', base.strip()) \
                .replace('|', '') \
                .replace('’', '')
+
 
 @register.filter
 def get_item(dictionary, key):
@@ -25,5 +24,5 @@ def get_item(dictionary, key):
 
 
 @register.filter
-def highlight(text, search):
-    return text.replace(search, '<span class="bg-warning">' + search + '</span>')
+def highlight_rus(text):
+    return re.sub(r'([А-яЁё])', r'<span style="color: #82551e">\1</span>', text)
