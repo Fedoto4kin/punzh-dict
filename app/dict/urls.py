9341f8fc99bc59626bf0eb2e8c6c2edf87d7f23e
FAnatole <fedoto4kin@gmail.com>
2021-04-25 22:10:10 +0300

    Fix urls

--- app/dict/urls.py
+++ app/dict/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from . import views
+from .models import KRL_ABC
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # TODO: combine simular
+    # TODO: combine similar
     path('search', views.search_proc, name='search_proc'),
     path('search/', views.search_proc, name='search_proc'),
-    path('search/<query>', views.search, name='search'),
-    path('search/<query>/<int:page>', views.search, name='search'),
-    path('<letter>', views.index, name='index'),
-    path('<letter>/<int:page>', views.index, name='index'),
+    path('search/<str:query>', views.search, name='search'),
+    path('search/<str:query>/<int:page>', views.search, name='search'),
+    path('<letter>/', views.index, name='index'),  # TODO: filter letter one char in KRL
+    path('<letter>/<int:page>', views.index, name='index'),  # TODO: filter letter one char in KRL
 ]
