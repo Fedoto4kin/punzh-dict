efabad2fe38133bab79bcb7ebbfe8ccae78b6d04
FAnatole <fedoto4kin@gmail.com>
2021-11-16 21:05:48 +0300

    Add preview

--- app/dict/admin.py
+++ app/dict/admin.py
@@ -8,11 +8,10 @@ from .models import Article
 
 class ArticleAdm(admin.ModelAdmin):
 
-    fields = ('_word', 'word_normalized', 'word', 'article_html')
-
+    fields = ('_word', 'word_normalized', 'word', 'article_html', '_article_html')
     list_display = ('_word', '_article_html')
 
-    readonly_fields = ["_word"]
+    readonly_fields = ["_word", '_article_html']
 
     search_fields = ('word',)
     exclude = ("first_letter", "text_search", 'first_trigram')
@@ -20,12 +19,10 @@ class ArticleAdm(admin.ModelAdmin):
 
     def _article_html(self, obj):
         return format_html(obj.article_html)
-
     _article_html.short_description = 'Словарная статья'
 
     def _word(self, obj):
         return format_html("<b>{word}</b>", word=normalization(obj.word))
-
     _word.short_description = 'Заголовок (в норм. орф.)'
 
 
