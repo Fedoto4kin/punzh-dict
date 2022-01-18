02b73b8b39f252d3e5c14874142824d750db409e
Anatole F <fedoto4kin@gmail.com>
2022-01-18 12:12:45 +0300

    create source admin + fixtures

--- app/dict/admin.py
+++ app/dict/admin.py
@@ -3,7 +3,7 @@ from dict.services import normalization
 from django.utils.html import format_html, html_safe
 
 
-from .models import Article, ArticleIndexTranslate
+from .models import Article, ArticleIndexTranslate, Source
 
 class TranslateInline(admin.TabularInline):
     extra = 0
@@ -12,28 +12,30 @@ class TranslateInline(admin.TabularInline):
 
 class ArticleAdm(admin.ModelAdmin):
 
-    fields = ('_word', 'word_normalized', 'word', 'article_html', '_article_html')
-    list_display = ('_word', '_article_html')
+    fields = ('_word', 'word_normalized', 'word',
+              'article_html', '_article_html', 'source',
+              'source_detalization',)
+    list_display = ('_word', '_article_html',)
 
-    readonly_fields = ["_word", '_article_html']
+    readonly_fields = ["_word", '_article_html',]
 
     search_fields = ('word',)
     exclude = ("first_letter", "text_search", 'first_trigram')
 
     inlines = [
-        TranslateInline,
+        TranslateInline
     ]
-
-
 
     def _article_html(self, obj):
         return format_html(obj.article_html)
+
     _article_html.short_description = 'Словарная статья'
 
     def _word(self, obj):
         if obj.word_normalized:
             return format_html("<s>{word}</s> <b>{word_norm}</b>", word=normalization(obj.word), word_norm=obj.word_normalized)
         return format_html("<b>{word}</b>", word=normalization(obj.word))
+
     _word.short_description = 'Заголовок (в норм. орф.)'
 
 
