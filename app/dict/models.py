65351cd4400e142379e37540886634538bb39cf7
Anatole F <fedoto4kin@gmail.com>
2022-01-18 12:38:23 +0300

    Source model name

--- app/dict/models.py
+++ app/dict/models.py
@@ -13,11 +13,16 @@ class Source(models.Model):
                                    blank=True,
                                    null=True,
                                    verbose_name='Описание')
-    css = models.CharField(max_length=255, blank=True, null=True,)
+    css = models.CharField(max_length=255, blank=True, null=True, verbose_name='CSS Styling')
 
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Источник'
+        verbose_name_plural = 'Источники'
+        ordering = ['id']
 
 
 class Article(models.Model):
