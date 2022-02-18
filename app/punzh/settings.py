4216700f7a44b942f9806b322745cdfac52d0c9f
Anatole F <fedoto4kin@gmail.com>
2022-02-18 19:50:42 +0300

    local static serve fix: test on production

--- app/punzh/settings.py
+++ app/punzh/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,9 @@ USE_TZ = True
 
 STATIC_ROOT = '/app/static/'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
