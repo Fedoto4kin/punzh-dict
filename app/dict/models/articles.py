5b71f7a88d48b772d28fdc8467d0d54e0156a09a
Anatole F <fedoto4kin@gmail.com>
2022-02-20 11:19:02 +0300

    move out source model;

--- app/dict/models/articles.py
+++ app/dict/models/articles.py
@@ -3,27 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
+from .source import Source
 from ..helpers import gen_word_variants, create_ngram, KRL_ABC, normalization
-
-class Source(models.Model):
-
-    title = models.CharField(max_length=255,
-                             verbose_name='Название источника')
-    description = models.TextField(default='',
-                                   blank=True,
-                                   null=True,
-                                   verbose_name='Описание')
-    css = models.CharField(max_length=255, blank=True, null=True, verbose_name='CSS Styling')
-
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Источник'
-        verbose_name_plural = 'Источники'
-        ordering = ['id']
-
 
 class Article(models.Model):
 
