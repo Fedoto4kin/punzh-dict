ba3e04532222c6add32ca7a88a14691ad7ba8813
Anatole F <fedoto4kin@gmail.com>
2022-10-28 22:11:14 +0300

    sorting by id in admin panel

--- app/dict/admin.py
+++ app/dict/admin.py
@@ -1,9 +1,11 @@
 import re
-from django.contrib import admin
-from django.utils.html import format_html, html_safe
 
-from .models import Article, ArticleIndexTranslate, ArticleIndexWord, Source
+from django.contrib import admin
+from django.utils.html import format_html
+
 from .helpers import normalization
+from .models import Article, ArticleIndexTranslate, Source, ArticleIndexWord
+
 
 class TranslateInline(admin.TabularInline):
     extra = 0
@@ -20,9 +22,11 @@ class ArticleAdm(admin.ModelAdmin):
     fields = ('_word', 'word_normalized', 'word',
               'article_html', '_article_html', 'source',
               'source_detalization', 'linked_article')
-    list_display = ('_word', '_article_html',)
 
+    list_display = ('id', '_word', '_article_html',)
     readonly_fields = ["_word", '_article_html',]
+
+    sorting = ['id',]
 
     search_fields = ('word',)
     exclude = ("first_letter", "text_search", 'first_trigram')
@@ -33,17 +37,26 @@ class ArticleAdm(admin.ModelAdmin):
 
     def get_search_results(self, request, queryset, search_term):
 
-        search = re.sub(r'[^\w\-\s\.\?]', '', search_term) \
+        search_term = re.sub(r'[^\w\-\s\.\?]', '', search_term) \
             .replace('š', 's') \
             .replace('č', 'c') \
-            .replace('ž', 'z') \
-            .replace('?', '%') \
-            .replace('.', '_')
+            .replace('ž', 'z')
 
-        pks0 = ArticleIndexWord.objects.filter(word__istartswith=search).values_list('article_id', flat=True)
-        queryset = Article.objects.filter(pk__in=pks0).all()
+        queryset, use_distinct = super(ArticleAdm, self).get_search_results(request, queryset, search_term)
+        try:
+            search_term_as_int = int(search_term)
+        except ValueError:
+            pass
+        else:
+            queryset |= self.model.objects.filter(age=search_term_as_int)
+        return queryset, use_distinct
 
-        return queryset, False
+    # def get_search_results(self, request, queryset, search_term):
+    #
+    #     pks0 = ArticleIndexWord.objects.filter(word__istartswith=search).values_list('article_id', flat=True)
+    #     queryset = Article.objects.filter(pk__in=pks0).all()
+    #
+    #     return queryset, False
 
 
     def _article_html(self, obj):
@@ -57,3 +70,4 @@ class ArticleAdm(admin.ModelAdmin):
         return format_html("<b>{word}</b>", word=normalization(obj.word))
 
     _word.short_description = 'Заголовок (в норм. орф.)'
+
