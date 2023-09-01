04f16bcbc0f27444da2886677aa1344026c006e0
Anatole F <fedoto4kin@gmail.com>
2023-09-02 02:52:23 +0300

    Refactoring: views

--- app/dict/urls.py
+++ app/dict/urls.py
@@ -4,20 +4,22 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # TODO: combine similar
-    path('about/', views.AboutPageStaticView.as_view(), name='about'),
-    path('intro/', views.IntroStaticView.as_view(), name='intro'),
-    path('punzhina/', views.PunzhStaticView.as_view(), name='punzh'),
-    path('dialects/', views.DialectsStaticView.as_view(), name='dialects'),
-    path('team/', views.TeamStaticView.as_view(), name='team'),
+    path('about/', views.StaticView.as_view(template_name='staticPages/about.html'), name='about'),
+    path('intro/', views.StaticView.as_view(template_name='staticPages/intro.html'), name='intro'),
+    path('punzhina/', views.StaticView.as_view(template_name='staticPages/punzh.html'), name='punzh'),
+    path('dialects/', views.StaticView.as_view(template_name='staticPages/dialects.html'), name='dialects'),
+    path('team/', views.StaticView.as_view(template_name='staticPages/team.html'), name='team'),
+
     path('search', views.search_proc, name='search_proc'),
     path('search/', views.search_proc, name='search_proc'),
     path('search/<str:query>', views.search, name='search'),
     path('search/<str:query>/<int:page>', views.search, name='search'),
+
     path('tags/', views.tag_search, name='tags'),
     path('tags/<str:tags>', views.tag_search, name='tags'),
     path('tags/<str:tags>/<int:page>', views.tag_search, name='tags'),
+
+    path('', views.index, name='index'),
     path('<letter>/', views.index, name='index'),  # TODO: filter letter one char in KRL
     path('<letter>/<int:page>', views.index, name='index'),
 ]
